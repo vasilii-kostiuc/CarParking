c53b90dad3b68ce412621ffeb885f536d4ad3e2c
vasea <vasile.costiuc@it-lab.md>
2023-03-07 18:18:27 +0200

    Auth Controller added

---
+++ CarParking.Api/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using CarParking.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarParking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register()
+        {
+            
+        }
+
+    }
+}
