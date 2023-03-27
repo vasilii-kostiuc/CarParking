eebeab8af499d3e3b0beeb7118fd762619ad663c
vasea <vasile.costiuc@it-lab.md>
2023-03-27 18:44:55 +0300

    Parking zones controller added

--- CarParking.Api.Models/Models/LoginRequest.cs
+++ CarParking.Api.Models/Models/LoginRequest.cs
@@ -1,9 +1,12 @@
-﻿namespace CarParking.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarParking.Api.Models
 {
     public class LoginRequest
     {
+        [Required]
         public string Email { get; set; }
-
+        [Required]
         public string Password { get; set; }
     }
 }
--- CarParking.Api.Models/Models/RegistrationRequest.cs
+++ CarParking.Api.Models/Models/RegistrationRequest.cs
@@ -1,13 +1,16 @@
-﻿namespace CarParking.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarParking.Api.Models
 {
     public class RegistrationRequest
     {
+        [Required]
         public string Name { get; set; }
-
+        [Required]
         public string Email { get; set; }
-
+        [Required]
         public string Password { get; set; }
-
+        [Required]
         public string Confirmation { get; set; }
     }
 }
